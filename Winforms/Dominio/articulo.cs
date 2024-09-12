9e4b74070f593cfd6bd8502ed5b226d4f1c54d67
AgustinHenin <168901916+AgustinHenin@users.noreply.github.com>
2024-09-12 03:18:16 -0300

    Add files via upload

---
+++ Winforms/Dominio/articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set;}
+        public Marca marca { get; set; }
+        public Categoria categoria { get; set; }
+        public float precio { get; set; }
+        public string imagenurl { get; set; }
+
+    }
+}
